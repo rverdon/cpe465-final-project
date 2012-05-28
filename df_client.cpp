e57da835affa1eb4242b997a592c4c53ad08a008
Ryan Verdon <ryan.verdon@gmail.com>
2012-05-28 13:00:43 -0700

    Finished multicast code

--- df_client.cpp
+++ df_client.cpp
@@ -26,6 +26,9 @@
 #include <map>
 #include <algorithm> 
 
+#define MAX_PACKET_SZ    1024
+#define MULTICAST_PORT   60001
+#define MULTICAST_GRP    "225.0.0.6"
 
 using namespace std;
 
@@ -37,8 +40,60 @@ using namespace std;
 int
 main(int argc, char *argv[])
 {
- 
-    return EXIT_SUCCESS;
+    unsigned char *packet_buffer = NULL;
+    uint32_t sk                  =  0;
+    uint32_t nbytes              =  0;
+    uint32_t addrlen             =  0;
+    uint32_t yes                 =  1;
+    struct   ip_mreq mreq;
+    struct   sockaddr_in addr;
+
+    if ((sk = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("df_client.cpp, socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Allow multiple sockets to use the same port
+    if (setsockopt(sk, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+        perror("df_client.cpp, setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(MULTICAST_PORT);
+    addrlen              = sizeof(addr);
+
+    if (bind(sk, (struct sockaddr *) &addr, addrlen) < 0) {
+        perror("df_client.cpp, bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Use setsockopt() to join multicast group
+    mreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_GRP);
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    if (setsockopt(sk, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+        perror("df_client.cpp, setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get a new buffer from our allocator
+    packet_buffer = (unsigned char*) malloc(MAX_PACKET_SZ);
+
+    while (1) {
+        // Clear the buffer
+        memset(packet_buffer, 0, MAX_PACKET_SZ);
+
+        if ((nbytes = recvfrom(sk, packet_buffer, MAX_PACKET_SZ, 0, (struct sockaddr *) &addr, &addrlen)) < 0) {
+            perror("df_client.cpp, recvfrom");
+            exit(EXIT_FAILURE);
+        }
+        printf("Recieved %d bytes from %s\n", nbytes, inet_ntoa(addr.sin_addr));
+    }
+
+    free(packet_buffer);
+    return 0;
 }
 
 
--- df_server.cpp
+++ df_server.cpp
@@ -26,6 +26,10 @@
 #include <map>
 #include <algorithm> 
 
+#define MAX_PACKET_SZ    1024
+#define MULTICAST_PORT   60001
+#define MULTICAST_GRP    "225.0.0.6"
+
 
 using namespace std;
 
@@ -37,8 +41,37 @@ using namespace std;
 int
 main(int argc, char *argv[])
 {
- 
-    return EXIT_SUCCESS;
+    unsigned char* buffer      = NULL;
+    size_t              psz    = 0;
+    uint32_t            sk     = 0;
+    struct sockaddr_in  addr;
+
+    if(argc < 1)
+    {
+        printf("ERROR: ./df_server <file name>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((sk = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("df_server.cpp, socket");
+        exit(EXIT_FAILURE);
+    }
+
+    addr.sin_family      = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(MULTICAST_GRP);
+    addr.sin_port        = htons(MULTICAST_PORT);
+
+    // This buffer is big enough to hold the biggest packet
+    psz     = MAX_PACKET_SZ;
+    buffer  = (unsigned char*)malloc(psz);
+
+    if (sendto(sk, buffer, psz, 0, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        perror("df_server.h, join, sendto");
+        exit(EXIT_FAILURE);
+    }
+            
+    free(buffer);
+    return 0;
 }
 
 
